98a910be3aa791d01150259d973b39fa80d3d2ce
Oswaldo Yanez <oswaldo.business31@gmail.com>
2021-08-16 13:57:01 -0400

    upgrade day 02.

--- day2.py
+++ day2.py
@@ -2,8 +2,6 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-#HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
-#HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
 
 
 print("Welcome to the tip Calculator")
