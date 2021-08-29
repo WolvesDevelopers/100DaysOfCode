909c052d997f9bdf44192efda113de3b00a18eb7
Oswaldo Yanez <oswaldo.business31@gmail.com>
2021-08-29 17:36:53 -0400

    day 05: upload password generator.

---
+++ day5.py
@@ -0,0 +1,56 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+# aux_list = []
+# for nr_letter in range(nr_letters):
+#   choice_letters = random.choice(letters)
+#   aux_list.append(choice_letters)
+
+# for nr_symbol in range(nr_symbols):
+#   choice_symbols = random.choice(symbols)
+#   aux_list.append(choice_symbols)
+
+# for nr_number in range(nr_numbers):
+#   choice_numbers = random.choice(numbers)
+#   aux_list.append(choice_numbers)
+
+# password = ""
+# for char in aux_list:
+#   password += char
+
+# print(f"Your password is: {password}")
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+aux_list = []
+for nr_letter in range(nr_letters):
+  choice_letters = random.choice(letters)
+  aux_list.append(choice_letters)
+
+for nr_symbol in range(nr_symbols):
+  choice_symbols = random.choice(symbols)
+  aux_list.append(choice_symbols)
+
+for nr_number in range(nr_numbers):
+  choice_numbers = random.choice(numbers)
+  aux_list.append(choice_numbers)
+
+random.shuffle(aux_list)
+
+password = ""
+for char in aux_list:
+  password += char
+
+print(f"Your password is: {password}")
